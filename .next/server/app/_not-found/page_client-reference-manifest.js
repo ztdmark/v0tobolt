57c025f1f68d44ce8989432d6441979dfc5cc428
ztdmark <relief.fund.e4e@gmail.com>
2025-06-13 20:52:19 +0800

    Updated app-build-manifest.json

---
+++ .next/server/app/_not-found/page_client-reference-manifest.js
@@ -0,0 +1,1 @@
+globalThis.__RSC_MANIFEST=(globalThis.__RSC_MANIFEST||{});globalThis.__RSC_MANIFEST["/_not-found/page"]={"moduleLoading":{"prefix":"/_next/"},"ssrModuleMapping":{},"edgeSSRModuleMapping":{},"clientModules":{},"entryCSSFiles":{"/home/project/":[],"/home/project/app/_not-found/page":[]},"rscModuleMapping":{},"edgeRscModuleMapping":{}}
